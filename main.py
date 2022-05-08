f52e17bc66f4d13f135d3d2d440a9e735b635c76
Kamyab Azizi <95940601+kamyabazizi@users.noreply.github.com>
2022-05-08 14:50:14 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pickle
 import time
 import tqdm
-
+import os
 import numpy as np
 import torch
-
+import torchvision.transforms as transforms
+from utils.util import save_image, tensor2im
 from data import create_dataloader
 from models import create_model
 from IPython.display import display
 from PIL import Image
-import torchvision.transforms as transforms
-from utils.util import save_image, tensor2im
+
 
 #Get the dataloader for evaluation
 with open('opts/opt_full.pkl', 'rb') as f:
@@ -25,7 +25,6 @@ model_full.setup(opt, verbose=False)
 
 transform_list = [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 transform = transforms.Compose(transform_list)
-
 A_dir = 'imgs/maps/A'
 B_dir = 'imgs/maps/B'
 
@@ -40,5 +39,5 @@ for file in files:
     input = transform(A_img).to('cuda:0')
     input = input.reshape([1, 3, 256, 256])
     output_full = model_full.netG(input).cpu()
-    B_full = tensor2im(output_full)
-    save_image(B_full, 'output/full/%s.png' % base, create_dir=True)
+    B_full = tensor2im(output_full)  
+    save_image(B_full, 'output/full/%s.png' % base, create_dir=True) 
