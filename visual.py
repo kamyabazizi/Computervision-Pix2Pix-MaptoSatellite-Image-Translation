8ced63a753634e19f1e43794ba85e3f5288840fc
Kamyab Azizi <95940601+kamyabazizi@users.noreply.github.com>
2022-05-08 14:43:43 +0430

    Update visual.py

--- visual.py
+++ visual.py
@@ -20,25 +20,4 @@ for file in files:
     all.paste(p1, (0, 0))
     all.paste(p2, (256, 0))
     all.paste(p3, (256*2, 0))
-    display(all)from PIL import Image
-A_dir = 'imgs/maps/A'
-B_dir = 'imgs/maps/B'
-files = os.listdir(A_dir)
-print('             Input                        Original                        Ground-truth')
-
-for file in files:
-    if not file.endswith('.PNG'):
-        continue
-    base = file.split('.')[0]
-    A_path = os.path.join(A_dir, file)
-    
-    B_path = os.path.join(B_dir, file)
-    
-    p1 = Image.open(A_path)
-    p2 = Image.open('output/full/%s.png' % base)
-    p3 = Image.open(os.path.join(B_path))
-    all = Image.new('RGB', (256*3, 256))
-    all.paste(p1, (0, 0))
-    all.paste(p2, (256, 0))
-    all.paste(p3, (256*2, 0))
     display(all)
